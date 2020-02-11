6c4b4e314e5aa31f54c22a86c0b4ab934eeb38c4
thaothai <thaithao69@gmail.com>
2020-02-11 18:52:47 +0700

    update for suit 2

---
+++ demoframework/src/test/java/com/abc/testCases/TC_Login_08.java
@@ -0,0 +1,13 @@
+package com.abc.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_Login_08 extends BaseClass{
+
+	@Test
+	public void TCLogin02() {
+		Assert.assertTrue(true);
+		Assert.assertTrue(true);
+	}
+}
